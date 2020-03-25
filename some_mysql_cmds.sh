9fc09d301cd337960f9a782be51bcce26ed284c9
Alexey <usharik@mail.ru>
2020-03-25 20:44:52 +0100

    Create some_mysql_cmds.sh

---
+++ some_mysql_cmds.sh
@@ -0,0 +1,8 @@
+# https://stackoverflow.com/questions/4444861/pid-error-on-mysql-server-start
+/usr/local/mysql/support-files/mysql.server start
+/usr/local/mysql/support-files/mysql.server stop
+/usr/local/mysql/support-files/mysql.server restart
+
+ls /usr/local/mysql/data/
+ls /usr/local/var/mysql/
+
